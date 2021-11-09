e93f633965dbe94745427ab572e7296bc13739f5
Soyda <stevendruesne@gmail.com>
2021-11-09 10:29:47 +0100

    exo test correction

--- exo_test.py
+++ exo_test.py
@@ -4,25 +4,26 @@ def my_sort(liste):
 
     for i in range(len(liste)-1) :
         if type(liste[i]) != type(liste[i+1]) :
-            return "ERROR: Elements should be of the same type"
+            raise(TypeError("ERROR: Elements should be of the same type"))
     
 
     for j in range(len(liste)-1) :
         for k in range(len(liste)-1) :
             if liste[k] > liste[k+1] :
-                temp1 = liste[k]
-                temp2 = liste[k+1]
-                liste.remove(temp1)
-                liste.remove(temp2)
-                liste.insert(k, temp2)
-                liste.insert(k+1, temp1)
+                # temp1 = liste[k]
+                # temp2 = liste[k+1]
+                # liste.remove(temp1)
+                # liste.remove(temp2)
+                # liste.insert(k, temp2)
+                # liste.insert(k+1, temp1)
+                liste[k], liste[k+1] = liste[k+1], liste[k]
                 # print(liste)
     # print(liste)
     return liste
 
 
-print(my_sort([9, 8, 7, 6, 5, 4, 3, 2, 1]))
-print(my_sort(['g', 'p', 'l', 'a', 'e', 'b', 'z', 'c', 'h']))
-print(my_sort([True, False, True, False, True, False, True, False, False]))
-print(my_sort([9, 8, '7', 6, 5, 4, 3, 2, 1]))
-print(my_sort([9, 8.5, 7, 6, 5.2, 4, 3, 2, 1]))
+# print(my_sort([9, 8, 7, 6, 5, 4, 3, 2, 1]))
+# print(my_sort(['g', 'p', 'l', 'a', 'e', 'b', 'z', 'c', 'h']))
+# print(my_sort([True, False, True, False, True, False, True, False, False]))
+# print(my_sort([9, 8, '7', 6, 5, 4, 3, 2, 1]))
+# print(my_sort([9, 8.5, 7, 6, 5.2, 4, 3, 2, 1]))
--- test_exo_test.py
+++ test_exo_test.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import inspect
-# import pytest
+import pytest
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -13,8 +13,13 @@ def test_my_sort():
     assert my_sort([3,4,1,2,1]) == [1,1,2,3,4]
     assert my_sort([3,2,5,3]) == [2,3,3,5]
     assert my_sort([3.5, 2.9 , 2.5, 3.9]) == [2.5, 2.9, 3.5, 3.9]
-    assert my_sort([1.5, 5, 8, 2.2]) == "ERROR: Elements should be of the same type"
-    assert my_sort(['1.5', 5, 8, 2.2]) == "ERROR: Elements should be of the same type"
-    assert my_sort(['g', 5, 8, 2.2]) == "ERROR: Elements should be of the same type"
+    # assert my_sort([1.5, 5, 8, 2.2]) == "ERROR: Elements should be of the same type"
+    # assert my_sort(['1.5', 5, 8, 2.2]) == "ERROR: Elements should be of the same type"
+    # assert my_sort(['g', 5, 8, 2.2]) == "ERROR: Elements should be of the same type"
     assert my_sort([True, False, True, False]) == [False, False, True,True]
-    assert my_sort([3, True, 5, 3]) == "ERROR: Elements should be of the same type"
+    # assert my_sort([3, True, 5, 3]) == "ERROR: Elements should be of the same type"
+    with pytest.raises(TypeError("ERROR: Elements should be of the same type")) :
+        my_sort([1.5, 5, 8, 2.2])
+        my_sort(['1.5', 5, 8, 2.2])
+        my_sort(['g', 5, 8, 2.2])
+        my_sort([3, True, 5, 3])
